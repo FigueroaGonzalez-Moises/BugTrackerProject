3f97b1440f5cf016a3e6d84858b3bbc2d27c4be8
YashiroSennzai <61809595+YashiroSennzai@users.noreply.github.com>
2020-09-07 21:04:17 -0700

    Updates

---
+++ precache-manifest.af934959dcb3c356fc29db6ede9f7351.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd8567acf6a6b018064194eabde42f3",
+    "url": "/BugTrackerProject/index.html"
+  },
+  {
+    "revision": "1c34a605d34461e9c0e0",
+    "url": "/BugTrackerProject/static/css/2.53159467.chunk.css"
+  },
+  {
+    "revision": "bfa42a90068de793034a",
+    "url": "/BugTrackerProject/static/css/main.7f6d183d.chunk.css"
+  },
+  {
+    "revision": "1c34a605d34461e9c0e0",
+    "url": "/BugTrackerProject/static/js/2.9a1f7dfc.chunk.js"
+  },
+  {
+    "revision": "597a63747f318a9e58a5559c0456a793",
+    "url": "/BugTrackerProject/static/js/2.9a1f7dfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa42a90068de793034a",
+    "url": "/BugTrackerProject/static/js/main.73c3f1a0.chunk.js"
+  },
+  {
+    "revision": "ae48af740c77c7cba8f9",
+    "url": "/BugTrackerProject/static/js/runtime-main.2e5ba3e5.js"
+  },
+  {
+    "revision": "21ab141391bbe1038da2c97ff48640a0",
+    "url": "/BugTrackerProject/static/media/black-rock-tiles.21ab1413.jpg"
+  },
+  {
+    "revision": "a812fbf51b2d303132be5e87cde63cfe",
+    "url": "/BugTrackerProject/static/media/unnamed.a812fbf5.webp"
+  }
+]);
